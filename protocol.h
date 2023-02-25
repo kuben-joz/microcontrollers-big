d6149b96d9dc69c830c8e0699c606ec2a28f6972
Jakub Jozefowicz <kuben.joz@gmail.com>
2023-02-25 21:19:52 +0100

    before adding bluetooth

--- protocol.h
+++ protocol.h
@@ -1,13 +1,15 @@
 typedef enum SendCode {
     TEMP_VAL,
-    WEIGHT_VAL
+    WEIGHT_VAL,
+    ADC_VAL
 } SendCode;
 
 enum RecieveCode {
-    CALIBRATION_START,
-    CALIBRATION_VAL,
-    CALIBRATION_END,
+    ADC_REQ,
     TEMP_REQ,
     WEIGHT_REQ,
-    TARE_REQ
+    TARE_REQ,
+    MONOMIAL_0,
+    MONOMIAL_1,
+    MONOMIAL_2
 } RecieveCode;
